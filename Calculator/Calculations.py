dfcaf531a40325570819011c1c257e194f214bf9
Michaelkane428 <michaelkane428@gmail.com>
2017-09-12 20:54:10 +0100

    Backend of the calculator working.
    Need to add the Frontend GUI.

---
+++ Calculator/Calculations.py
@@ -0,0 +1,16 @@
+class Calculations(object):
+    def __init__(self, num1=0, num2=0):
+        self.num1 = num1
+        self.num2 = num2
+
+    def addition(self, num1, num2):
+        return num1 + num2
+
+    def subtraction(self, num1, num2):
+        return num1 - num2
+
+    def division(self, num1, num2):
+        return num1 / num2
+
+    def multiplication(self, num1, num2):
+        return num1 * num2
---
+++ Calculator/__init__.py
@@ -0,0 +1,10 @@
+from Calculator.Calculations import Calculations as Calculations
+
+
+def main():
+    my_sum = Calculations()
+    print(my_sum.addition(1, 1))
+
+
+if __name__ == '__main__':
+    main()
